82a5ea99c61e165a3596005f2a8a963775aa9e0c
ArteM201000 <140517288+ArteM201000@users.noreply.github.com>
2026-07-09 16:13:43 +0300

    .

--- pymata-firmata_from_app.py
+++ pymata-firmata_from_app.py
@@ -7,18 +7,13 @@ board = pymata4.Pymata4("COM6", 57600)
 
 servo = Servo(9, board)
 
-path = Path("C:/Users/admin/Downloads/firmata/angle.txt")
+path = Path("angle.txt")
 
-servo.write_servo(0)
-time.sleep(1.5)
-servo.write_servo(180)
-time.sleep(1)
 while True:
     angle = path.read_text()
-    path_block = Path("C:/Users/admin/Downloads/firmata/block.txt")
+    path_block = Path("block.txt")
     block = path_block.read_text()
     if block == "True":
-        print("Поворачиваю...")
         servo.write_servo(angle)
-        time.sleep(1)
+        time.sleep(0.5)
         path_block.write_text("False")
