a8cadbb1a0d5be7391f5ce61502005c3faf67078
ArteM201000 <140517288+ArteM201000@users.noreply.github.com>
2026-07-10 15:20:56 +0300

    .

--- firmata_app.py
+++ firmata_app.py
@@ -1,28 +1,29 @@
 import streamlit as st
-from pathlib import Path
+from supabase import create_client
 
-path = Path("angle.txt")
-path_block = Path("block.txt")
+url = "https://wmzpahjpjkejfrzbewfz.supabase.co"
+key = "sb_publishable_O8Erz_dPQk3fBkmhopdY-g_UitSlzJZ"
+
+supabase = create_client(url, key)
+data = supabase.table("arduino_state").select("*").execute()
 
 st.header("Управление сервоприводом")
 
 angle = st.sidebar.radio("Выбери градус поворота:", [0, 30, 60, 90, 120, 150, 180])
 
 if st.button("Повернуть"):
-    past_angle = path.read_text()
+    past_angle = data["angle"]
 
     def success_message():
         st.success(angle)
-        path.write_text(f"{angle}")
-        path_block.write_text("True")
+        supabase.table("arduino_state").update({"angle": angle}).eq("id", 1).execute()
+        supabase.table("arduino_state").update({"state": True}).eq("id", 1).execute()
 
     def error_message():
         st.error("Серво уже повернут на этот угол, выбери другой")
-        path_block.write_text("False")
+        supabase.table("arduino_state").update({"state": False}).eq("id", 1).execute()
 
     if past_angle == str(angle):
         error_message()
     else:
         success_message()
-
-    st.write(path.resolve())
